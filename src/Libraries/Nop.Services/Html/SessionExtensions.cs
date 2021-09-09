da8b52c46a1f28696fd5668fdcc9492537650ab1
skoshelev <sergey.k@nopcommerce.com>
2021-09-09 16:56:50 +0300

    Issue 3101. Code refactoring of  ExternalAuthorizerHelper class

---
+++ src/Libraries/Nop.Services/Html/SessionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Nop.Core.Http.Extensions;
+using Nop.Services.Authentication;
+
+namespace Nop.Services.Html
+{
+    /// <summary>
+    ///  Represents extensions of ISession
+    /// </summary>
+    public static partial class SessionExtensions
+    {
+        #region Methods
+
+        /// <summary>
+        /// Add error
+        /// </summary>
+        /// <param name="session">Session</param>
+        /// <param name="error">Error</param>
+        public static void AddErrorsToDisplay(this ISession session, string error)
+        {
+            var errors = session.Get<IList<string>>(NopAuthenticationDefaults.ExternalAuthenticationErrorsSessionKey) ?? new List<string>();
+            errors.Add(error);
+            session.Set(NopAuthenticationDefaults.ExternalAuthenticationErrorsSessionKey, errors);
+        }
+
+        /// <summary>
+        /// Retrieve errors to display
+        /// </summary>
+        /// <returns>Errors</returns>
+        public static IList<string> RetrieveErrorsToDisplay(this ISession session)
+        {
+            var errors = session.Get<IList<string>>(NopAuthenticationDefaults.ExternalAuthenticationErrorsSessionKey);
+
+            if (errors != null)
+                session.Remove(NopAuthenticationDefaults.ExternalAuthenticationErrorsSessionKey);
+
+            return errors;
+        }
+
+        #endregion
+    }
+}
