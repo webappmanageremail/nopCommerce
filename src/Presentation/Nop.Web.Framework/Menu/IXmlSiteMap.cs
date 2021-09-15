ef8cbd6e22c2872ede238efc80c085fa86a8b2ec
skoshelev <sergey.k@nopcommerce.com>
2021-09-15 16:36:40 +0300

    Issue 3101. Added some comments

--- src/Presentation/Nop.Web.Framework/Menu/IXmlSiteMap.cs
+++ src/Presentation/Nop.Web.Framework/Menu/IXmlSiteMap.cs
@@ -7,8 +7,16 @@ namespace Nop.Web.Framework.Menu
     /// </summary>
     public interface IXmlSiteMap
     {
+        /// <summary>
+        /// Root node
+        /// </summary>
         SiteMapNode RootNode { get; set; }
 
+        /// <summary>
+        /// Load sitemap
+        /// </summary>
+        /// <param name="physicalPath">Filepath to load a sitemap</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         Task LoadFromAsync(string physicalPath);
     }
 }
